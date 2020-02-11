09460844c363284f05a5c13d7f731bea8907add1
busrasariguzel <busra.sariguzel@codeimmersives.com>
2020-02-11 14:48:23 -0500

    fixed something

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,9 +3,9 @@ const router = express.Router()
 
 const userController = require('../controllers/userController');
 
-router.get('/getAllUsers', userController.getAllUsers);
+router.get('/', userController.getAllUsers);
 router.post('/register', userController.register);
 router.post('/login', userController.login);
-router.put('/update/:id',userController.updateProfile)
-router.delete('/delete/:id', userController.deleteProfile);
+router.put('/updateProfile/:id',userController.updateProfile)
+router.delete('/deleteProfile/:id', userController.deleteProfile);
 module.exports = router;
